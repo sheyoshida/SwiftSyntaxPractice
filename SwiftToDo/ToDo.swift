ff4956ed129a09f9c6bd9f6c307976e18ef3dceb
shenayoshida <sheyoshida@gmail.com>
2016-01-09 10:52:36 -0500

    complete

--- SwiftToDo/ToDo.swift
+++ SwiftToDo/ToDo.swift
@@ -10,18 +10,18 @@ import Foundation
 
 // return any positive integer greater than 1000
 func returnAnyPositiveIntegerGreaterThan1000() -> Int? {
-    return nil
+    return 1001
 }
 
 
 // return any negative float
 func returnAnyNegativeDouble() -> Double? {
-    return nil
+    return -1.0
 }
 
 // return a Bool
 func returnABoolean() -> Bool? {
-    return nil
+    return true
 }
 
 /* ======================= */
@@ -29,46 +29,50 @@ func returnABoolean() -> Bool? {
 
 // return the sum of integerX and integerY
 func returnTheSumOfIntegerXAndIntegerY(integerX: Int, integerY: Int) -> Int? {
-    return nil
+    return integerX + integerY
 }
 
 // return the product of x and 100
 func returnTheProductOfXAnd100(x: Double) -> Double? {
-    return nil
+    return x * 100
 }
 
 // return true if x is greater than 10
 // return false if x is less than or equal to 10
 func isGreaterThan10(x: Double) -> Bool? {
-   return nil
+    return x > 10 ? true : false // ternary operator
 }
 
 // return true if x is even
 // return false if x is odd
 func isEven(x: Int) -> Bool? {
-    return nil
+    return x % 2 == 0
 }
 
 
 // return the sum of all numbers between 0 and x
 // include 0 and x.
 func sumFrom0To(x:Int) -> Int? {
-    return nil
+    var answer = 0
+    for number in 0...x {
+      answer += number
+    }
+    return answer
 }
 
 // return the 5th item (index 4) from the provided array
 func get5thItem(arr: [AnyObject]) -> AnyObject? {
-    return nil
+    return arr[4]
 }
 
 // Given a dictionary, return the value for a key
 func returnValueForKey(dict: Dictionary<String,String>, key: String) -> String? {
-    return nil
+    return dict[key]
 }
 
 
 
 // Provided a closure( same as obj-c block) as a parameter, simply execute the closure
 func executeClosure(closure: ()->()) {
-
+    closure()
 }
